68969e85a0fbadca27fd048a46635a0ce8bd2ed9
kabiru254 <xyrisyt@gmail.com>
2026-04-21 09:56:50 +0300

    first commit

--- app/forms.py
+++ app/forms.py
@@ -1,15 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, BooleanField, EmailField, SelectField
-from wtforms.validators import DataRequired, Length, EqualTo 
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, EmailField, SelectField, TextAreaField
+from wtforms.validators import DataRequired, Length, EqualTo , Email
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Edit Profile')
 
 
 class RegisterForm(FlaskForm):
     """Register Form"""
     username = StringField('Username', validators=[DataRequired(), Length(1, 64)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo("password")])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Register')
+
 
 class LoginForm(FlaskForm):
     """Login Form"""
@@ -17,3 +25,9 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
+
+
+class PostForm(FlaskForm):
+    """Comment Form"""
+    body = TextAreaField('Body', validators=[DataRequired()])
+    submit = SubmitField('Post')
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,68 @@
 from app import app, db
-from app.models import User
-from flask_login import login_user, logout_user, login_required 
-from flask import render_template, flash, redirect, url_for
-from app.forms import RegisterForm, LoginForm
+from app.models import User, Post
+from datetime import datetime
+from flask_login import login_user, logout_user, login_required, current_user
+from flask import render_template, flash, redirect, url_for, request
+from app.forms import RegisterForm, LoginForm, EditProfileForm, PostForm
+
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
+@login_required
+def index():
+    """Index URL"""
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.body.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is live!')
+        return redirect(url_for('index'))
+    posts = Post.query.all()
+    return render_template(
+        'index.html',
+        title= 'Home',
+        form=form,
+        posts=posts)    
+    
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('profile', username=current_user.username))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template(
+        'edit_profile.html',
+        title = 'Edit Profile',
+        form=form)
+    
+
+@app.route('/<username>/profile')
+@login_required
+def profile(username):
+    """About Me URL"""
+    user= User.query.filter_by(username=username).first_or_404()
+    posts = current_user.posts.all()
+    return render_template(
+        'profile.html',
+        title='Profile',
+        user=user,
+        posts=posts)
+    
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 
 @app.route('/logout')
@@ -10,19 +70,13 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-@app.route('/')
-@app.route('/home')
-@login_required
-def index():
-    """Index URL"""
-    return render_template('index.html', title='Home Page')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     """Register URL"""
     form = RegisterForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data)
+        user = User(username=form.username.data, email=form.email.data)
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
